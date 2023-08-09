a4b2102ffc9cc2257ed27e3142269d315c9a9d79
Júnior Bocelli <juniorbocelli@gmail.com>
2023-08-09 13:16:31 -0300

    Alter server location

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 session_start();
 
 //Já que o .htaccess manda o usuário para a raiz sempre, podemos pegar o caminho apenas uma vez
-$_SESSION['root'] = 'C:/xampp2/htdocs/estudo/dw2-trabalho-php-2/';
+$_SESSION['root'] = 'C:/xampp2/htdocs/monte-sua-dieta/';
 
 //Agora que já temos o root, podemos usar esse valor para determinar o caminho relativo de cada arquivo
 include $_SESSION['root']."routes.php";
